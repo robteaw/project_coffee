719eac81f54b0ad167d6e187183648aa4ed190d4
slpatton2 <slpatton2@uh.edu>
2018-04-29 17:31:05 -0500

    Populate Sales

--- app/models/sale.rb
+++ app/models/sale.rb
@@ -1,4 +1,11 @@
 class Sale < ApplicationRecord
   belongs_to :order
   belongs_to :employee
+
+end
+
+
+def calculate_sales
+  total = order.sum(&:total_amount)
+  update_columns(total_sales: total)
 end
