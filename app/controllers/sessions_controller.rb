9ce64eb6e64e133a5075feb1cdf249124d8e0c53
slpatton2 <slpatton2@uh.edu>
2018-04-30 01:54:18 -0500

    Heroku 3

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,6 +1,6 @@
 class SessionsController < ApplicationController
-  skip_before_filter :verify_authenticity_token
-  skip_before_filter :authenticate_user!, :only => "reply", :raise => false
+  skip_before_action :verify_authenticity_token
+  skip_before_action :authenticate_user!, :only => "reply", :raise => false
 
   def new
   end
